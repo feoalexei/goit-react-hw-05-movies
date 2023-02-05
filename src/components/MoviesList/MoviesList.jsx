6e4037930fac368487de32865af85d0906126c32
feoalexei <feoalexei@ukr.net>
2023-02-06 00:59:51 +0200

    added rating for movies

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { fetchTrending } from 'services/movies-api';
 import { Box } from 'Box';
+import { FaStar } from 'react-icons/fa';
 
 const MoviesList = () => {
   const [trendList, setTrendList] = useState([]);
@@ -14,26 +15,43 @@ const MoviesList = () => {
 
   console.log(trendList);
 
+  const generateRating = rate => {
+    return Array(Math.floor(rate)).fill('*');
+  };
+
   return (
     <>
-      <ul>
+      <Box
+        as="ul"
+        display="grid"
+        gridGap={5}
+        gridTemplateColumns="repeat(auto-fit, minmax(200px, auto))"
+      >
         {trendList.map(movie => (
           <li key={movie.id}>
             <div>
-              <img
-                src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
-                alt={movie.title || movie.name}
-              />
               <Link to={`/movies/${movie.id}`}>
+                <img
+                  src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+                  alt={movie.title || movie.name}
+                />
                 {movie.title || movie.name}
-              </Link>
+              </Link>{' '}
               {movie.release_date && (
                 <span>({movie?.release_date.slice(0, 4)})</span>
               )}
+              <Box as="ul" display="flex">
+                {generateRating(movie.vote_average).map(star => (
+                  <li>
+                    <FaStar style={{ fill: 'darkgoldenrod' }} />
+                  </li>
+                ))}{' '}
+                ({movie.vote_average.toFixed(1)}/10)
+              </Box>
             </div>
           </li>
         ))}
-      </ul>
+      </Box>
     </>
   );
 };
