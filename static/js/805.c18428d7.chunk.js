c2567c9a3d527fb45e4794d8871e6ea7a222c79b
feoalexei <53433276+feoalexei@users.noreply.github.com>
2023-02-13 06:29:28 +0000

    Deploying to gh-pages from @ feoalexei/goit-react-hw-05-movies@0b739053e8146002c2caa08fc4845d3e9c9bca32 🚀

--- static/js/805.c18428d7.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{9805:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)("main",{children:(0,n.jsx)("h2",{children:"Sorry we couldn't find that page"})})}}}]);
-//# sourceMappingURL=805.c18428d7.chunk.js.map
